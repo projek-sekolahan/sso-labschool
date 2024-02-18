30655387e12e7f74a501772b58b413b041f77e49
bagus farouk <b46usf@gmail.com>
2024-02-18 22:22:15 +0700

    commit

--- application/models/ClientAPI.php
+++ application/models/ClientAPI.php
@@ -46,6 +46,7 @@
                 explode('.',$_SERVER['HTTP_HOST'])[0] => hash('sha1',$authKey),
                 'csrf_token'=> $csrf,
             );
+			var_dump($response); return false;
             try {
                 $response = $this->_client->post($url,
                     [
