c1c9995973ea3f6579fa3f5b53534c300e065876
bagus farouk <b46usf@gmail.com>
2024-02-20 12:01:04 +0700

    commit

--- system/libraries/Session/Session_driver.php
+++ system/libraries/Session/Session_driver.php
@@ -141,12 +141,12 @@ abstract class CI_Session_driver implements SessionHandlerInterface {
 	{
 		return setcookie(
 			$this->_config['cookie_name'],
-			NULL,
+			'',
 			1,
 			$this->_config['cookie_path'],
 			$this->_config['cookie_domain'],
 			$this->_config['cookie_secure'],
-			TRUE ?? ''
+			TRUE
 		);
 	}
 
