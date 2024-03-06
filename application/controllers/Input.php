b978cc66655b77121766fc56c943d583b1f4517b
bagus farouk <b46usf@gmail.com>
2024-03-06 10:50:45 +0700

    commit

--- application/controllers/Input.php
+++ application/controllers/Input.php
@@ -17,8 +17,8 @@ class Input extends CI_Controller {
 		echo json_encode(
             [
             'status'    => true,
-			'csrfHash'  => $this->security->get_csrf_hash(),
-			'info'      => 'csrf cookie created'.$this->input->cookie('ci_sso_csrf_cookie'),
+			// 'csrfHash'  => $this->security->get_csrf_hash(),
+			'info'      => 'csrf cookie created',
             ]
         );
 	}
