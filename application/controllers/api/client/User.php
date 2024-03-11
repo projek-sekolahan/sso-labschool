0ce696e9dd90b948c8cb92a0fd2e1334b8b6d940
bagus farouk <b46usf@gmail.com>
2024-03-11 21:45:04 +0700

    commit

--- application/controllers/api/client/User.php
+++ application/controllers/api/client/User.php
@@ -30,8 +30,7 @@ class User extends RestController {
         } else {
 			$decode = $this->_AuthToken->validateTimestamp($this->_paramToken['token'],$this->_paramToken[explode('.',$_SERVER['HTTP_HOST'])[0]]);
 			if (is_object($decode)) {
-				var_dump($decode != false,(now() > $decode->expired)); return false;
-				if ($decode != false && (now() < $decode->expired)) {
+				if ($decode != false && (now() > $decode->expired)) {
 					$encrypted	= $this->_AuthToken->encrypt($dtAPI['data'],$decode->apikey,$decode->session_hash);
 					$http       = RestController::HTTP_CREATED;
 					$output = $this->_AuthToken->generateToken($encrypted,$this->input->post(explode('.',$_SERVER['HTTP_HOST'])[0]));
