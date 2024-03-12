ed755a73c3d6adf87896192af273d045332bef44
bagus farouk <b46usf@gmail.com>
2024-03-12 22:13:04 +0700

    commit

--- application/models/AuthToken.php
+++ application/models/AuthToken.php
@@ -17,12 +17,15 @@ class AuthToken extends CI_Model {
                 if ($token != false && (now() < isset($token->expired))) {
                     return $token;
                 } else {
-					
-					var_dump($this->decrypt($token->data,hash('sha256',explode('.',$_SERVER['HTTP_HOST'])[1]),substr(hash('sha256',explode('.',$_SERVER['HTTP_HOST'])[1]), 0, 16))); return false;
-                    return $token;
+					return $this->generateToken(
+						$this->decrypt(
+							$token->data,
+							hash('sha256',explode('.',$_SERVER['HTTP_HOST'])[1]),
+							substr(hash('sha256',explode('.',$_SERVER['HTTP_HOST'])[1]), 0, 16)
+						),$key
+					);
                 }
             } else {
-				var_dump($token); return false;
                 return $token;
             }
         }
