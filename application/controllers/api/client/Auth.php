fd660f363e6342b0a4045a7b53c19896797824a4
bagus farouk <b46usf@gmail.com>
2024-03-12 12:33:42 +0700

    commit

--- application/controllers/api/client/Auth.php
+++ application/controllers/api/client/Auth.php
@@ -48,7 +48,7 @@ class Auth extends RestController {
                                 'session_hash'  => $decode->session_hash,
                                 'expired'       => $decode->expired,
                             );
-							var_dump(hash('sha256',$this->input->cookie('ci_sso_session')),substr(hash('sha256',$this->input->cookie('ci_sso_session')), 0, 16)); return false;
+							
 							$encrypted	= $this->_AuthToken->encrypt(json_encode($token_data),hash('sha256',$this->input->cookie('ci_sso_session')),substr(hash('sha256',$this->input->cookie('ci_sso_session')), 0, 16));
                             $tokenJWT	= $this->_AuthToken->generateToken(['data'=>$encrypted],$decode->apikey);
 							// $tokenJWT	= $this->_AuthToken->generateToken($token_data,$decode->apikey);
