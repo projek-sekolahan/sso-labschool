c460273b1a9c425f621629223c73964060430321
bagus farouk <b46usf@gmail.com>
2024-03-12 08:34:34 +0700

    commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -201,7 +201,8 @@ function parseJwt(token) {
     var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
         return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
     }).join(''));
-	console.log(JSON.parse(jsonPayload))
+
+	console.log(JSON.parse(jsonPayload).data)
     // return JSON.parse(jsonPayload);
 };
 
