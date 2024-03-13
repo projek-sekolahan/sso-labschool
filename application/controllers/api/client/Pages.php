439c2d90353c6a35d7904398f1c460d6497c6589
bagus farouk <b46usf@gmail.com>
2024-03-13 21:03:36 +0700

    commit

--- application/controllers/api/client/Pages.php
+++ application/controllers/api/client/Pages.php
@@ -30,7 +30,9 @@ class Pages extends RestController {
             $http   = RestController::HTTP_BAD_REQUEST;
             $output = $dtAPI['data'];
         } else {
+			var_dump($this->_paramToken['token']);
 			$decode = $this->_AuthToken->validateTimestamp($this->_paramToken['token'],$this->_paramToken[explode('.',$_SERVER['HTTP_HOST'])[0]]);
+			var_dump(json_encode($dtAPI['data']),$decode); return false;
 			if (is_object($decode)) {
 				$encrypted	= $this->_AuthToken->encrypt(json_encode($dtAPI['data']),hash('sha256', $decode->apikey),substr(hash('sha256', $decode->session_hash), 0, 16));
 				$http       = RestController::HTTP_CREATED;
