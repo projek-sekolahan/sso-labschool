4cbfa4d15fc45a9b02920956f27e2fec7faf790f
bagus farouk <b46usf@gmail.com>
2024-03-24 14:04:13 +0700

    commit

--- assets/js/pages/form-validation.init.js
+++ assets/js/pages/form-validation.init.js
@@ -93,7 +93,7 @@ function validateForm(a, t, i) {
 
 function save(a, e, i) {
 	e.preventDefault()
-	console.log(a, e, i);
+	console.log($("#" + a).attr("action"));
 	if (a=='form-login') {
 		var hash = hashPass();
 		key			= btoa($("#username").val() + ":" + hash);
